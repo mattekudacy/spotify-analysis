108ae055a59a6c64a81bf9da71786c1d2e416f81
Cyrus Mante <74973194+mattekudacy@users.noreply.github.com>
2021-12-14 09:01:41 +0800

    w/ modelling

---
+++ no.py
@@ -0,0 +1,604 @@
+no_ids = {
+  "items": [
+    {
+      "track": {
+        "id": "6epn3r7S14KUqlReYr77hA",
+        "name": "Baby"
+      }
+    },
+    {
+      "track": {
+        "id": "5ygDXis42ncn6kYG14lEVG",
+        "name": "Baby Shark"
+      }
+    },
+    {
+      "track": {
+        "id": "4l62h4tiuUwn7eD6hxMlVQ",
+        "name": "It's Everyday Bro"
+      }
+    },
+    {
+      "track": {
+        "id": "25gD4PQjxCLnwIY2n23aEl",
+        "name": "Flores"
+      }
+    },
+    {
+      "track": {
+        "id": "6habFhsOp2NvshLv26DqMb",
+        "name": "Despacito"
+      }
+    },
+    {
+      "track": {
+        "id": "5tq6gO0LuNjmo9SRT1UcMv",
+        "name": "Dame Tu Cosita"
+      }
+    },
+    {
+      "track": {
+        "id": "4fK6E2UywZTJIa5kWnCD6x",
+        "name": "Friday"
+      }
+    },
+    {
+      "track": {
+        "id": "3SdZWZ653w62Ah37iDo831",
+        "name": "How It Is (Wap Bap...)"
+      }
+    },
+    {
+      "track": {
+        "id": "03UrZgTINDqvnUMbbIMhql",
+        "name": "Gangnam Style (강남스타일)"
+      }
+    },
+    {
+      "track": {
+        "id": "0B3LaidsxjnBTUlKgKQQe5",
+        "name": "Sweatshirt"
+      }
+    },
+    {
+      "track": {
+        "id": "7EAwgBKCOZYZ7d1NLw2FII",
+        "name": "PG Song for Kids (I Play PG Everyday)"
+      }
+    },
+    {
+      "track": {
+        "id": "4dgkXOOCKsEKgBgZbAankr",
+        "name": "ПОСОСИ"
+      }
+    },
+    {
+      "track": {
+        "id": "0Bo5fjMtTfCD8vHGebivqc",
+        "name": "Axel F"
+      }
+    },
+    {
+      "track": {
+        "id": "6BqdNDLZ3Pdcly46pu6nwj",
+        "name": "I Am A Gummy Bear - The Gummy Bear Song"
+      }
+    },
+    {
+      "track": {
+        "id": "4NhDYoQTYCdWHTvlbGVgwo",
+        "name": "GOOBA"
+      }
+    },
+    {
+      "track": {
+        "id": "5eqiMMbaeUZ32Q7sS00H35",
+        "name": "Anaconda"
+      }
+    },
+    {
+      "track": {
+        "id": "43ZyHQITOjhciSUUNPVRHc",
+        "name": "Gucci Gang"
+      }
+    },
+    {
+      "track": {
+        "id": "09CtPGIpYB4BrO8qb1RGsF",
+        "name": "Sorry"
+      }
+    },
+    {
+      "track": {
+        "id": "2vwlzO0Qp8kfEtzTsCXfyE",
+        "name": "Wrecking Ball"
+      }
+    },
+    {
+      "track": {
+        "id": "4saklk6nie3yiGePpBwUoc",
+        "name": "Dynamite"
+      }
+    },
+    {
+      "track": {
+        "id": "63TyoT9Ee03rQxv8xmdJ2l",
+        "name": "RAP DEVIL"
+      }
+    },
+    {
+      "track": {
+        "id": "0qcr5FMsEO85NAQjrlDRKo",
+        "name": "Let It Go - From \"Frozen\"/Soundtrack Version"
+      }
+    },
+    {
+      "track": {
+        "id": "2y4lAQpi5VTNLu2ldeTdUH",
+        "name": "We Can't Stop"
+      }
+    },
+    {
+      "track": {
+        "id": "314bi2IzSFaBWmAfn1c0eu",
+        "name": "Frick Da Police"
+      }
+    },
+    {
+      "track": {
+        "id": "4W960ZptPsutzziVfweVOr",
+        "name": "The Gummy Bear Song Spanish (Yo Soy Tu Gominola)"
+      }
+    },
+    {
+      "track": {
+        "id": "41L3O37CECZt3N7ziG2z7l",
+        "name": "Yummy"
+      }
+    },
+    {
+      "track": {
+        "id": "7qiZfU4dY1lWllzX7mPBI3",
+        "name": "Shape of You"
+      }
+    },
+    {
+      "track": {
+        "id": "6PQtzyaHnx61E6KxbW4ytc",
+        "name": "Fight Kulüp"
+      }
+    },
+    {
+      "track": {
+        "id": "1P17dC1amhFzptugyAO7Il",
+        "name": "Look What You Made Me Do"
+      }
+    },
+    {
+      "track": {
+        "id": "5jrdCoLpJSvHHorevXBATy",
+        "name": "Dark Horse"
+      }
+    },
+    {
+      "track": {
+        "id": "5nqehpNGoRAz4wUFK3wT9k",
+        "name": "Me Solta"
+      }
+    },
+    {
+      "track": {
+        "id": "4Oun2ylbjFKMPTiaSbbCih",
+        "name": "WAP (feat. Megan Thee Stallion)"
+      }
+    },
+    {
+      "track": {
+        "id": "3OtMnyUaiipcAT23A8liyi",
+        "name": "Swish Swish"
+      }
+    },
+    {
+      "track": {
+        "id": "5KawlOMHjWeUjQtnuRs22c",
+        "name": "Boy With Luv (feat. Halsey)"
+      }
+    },
+    {
+      "track": {
+        "id": "4JUPEh2DVSXFGExu4Uxevz",
+        "name": "Ice Cream (with Selena Gomez)"
+      }
+    },
+    {
+      "track": {
+        "id": "0jTirSFGbl9fIhtqhxPAjW",
+        "name": "What Do You Mean?"
+      }
+    },
+    {
+      "track": {
+        "id": "6BpQPQL7fCaz4WUi6jpoBI",
+        "name": "Il Puccino Pio (En Español)"
+      }
+    },
+    {
+      "track": {
+        "id": "6hvczQ05jc1yGlp9zhb95V",
+        "name": "Kill This Love"
+      }
+    },
+    {
+      "track": {
+        "id": "4SFknyjLcyTLJFPKD2m96o",
+        "name": "How You Like That"
+      }
+    },
+    {
+      "track": {
+        "id": "4rHmKlFRiFzabiVO6e9w2e",
+        "name": "Bon appétit"
+      }
+    },
+    {
+      "track": {
+        "id": "3UGIZ8qcrMTwzLbx6Kttqt",
+        "name": "Boyfriend"
+      }
+    },
+    {
+      "track": {
+        "id": "2Fxmhks0bxGSBdJ92vM42m",
+        "name": "bad guy"
+      }
+    },
+    {
+      "track": {
+        "id": "0cqRj7pUJDkTCEsJkx8snD",
+        "name": "Shake It Off"
+      }
+    },
+    {
+      "track": {
+        "id": "5RIVoVdkDLEygELLCniZFr",
+        "name": "Watch Me (Whip / Nae Nae)"
+      }
+    },
+    {
+      "track": {
+        "id": "4S8d14HvHb70ImctNgVzQQ",
+        "name": "I Love It (& Lil Pump)"
+      }
+    },
+    {
+      "track": {
+        "id": "79l5JbcB7MVcjmYlvgq9KQ",
+        "name": "Boomerang"
+      }
+    },
+    {
+      "track": {
+        "id": "3iLUy0E1Jk5ayKG34Xb9XA",
+        "name": "Stupid Hoe"
+      }
+    },
+    {
+      "track": {
+        "id": "05DKZXWKDEml6fqqc3bqFl",
+        "name": "Freak Penne"
+      }
+    },
+    {
+      "track": {
+        "id": "0gzu5mm36VJH2Zqu8sQPTf",
+        "name": "Genda Phool (feat. Payal Dev)"
+      }
+    },
+    {
+      "track": {
+        "id": "4lQsB3ERTWSNaAN1IkuNRl",
+        "name": "DDU-DU DDU-DU"
+      }
+    },
+    {
+      "track": {
+        "id": "6F5c58TMEs1byxUstkzVeM",
+        "name": "Roar"
+      }
+    },
+    {
+      "track": {
+        "id": "2l8nEO0vsTRCRnGqQty6qx",
+        "name": "FEFE"
+      }
+    },
+    {
+      "track": {
+        "id": "5SE57ljOIUJ1ybL9U6CuBH",
+        "name": "DNA"
+      }
+    },
+    {
+      "track": {
+        "id": "2JzZzZUQj3Qff7wapcbKjc",
+        "name": "See You Again (feat. Charlie Puth)"
+      }
+    },
+    {
+      "track": {
+        "id": "32OlwWuMpZ6b0aN2RZOeMS",
+        "name": "Uptown Funk (feat. Bruno Mars)"
+      }
+    },
+    {
+      "track": {
+        "id": "4w8niZpiMy6qz1mntFA5uM",
+        "name": "Taki Taki (with Selena Gomez, Ozuna & Cardi B)"
+      }
+    },
+    {
+      "track": {
+        "id": "4ipnJyDU3Lq15qBAYNqlqK",
+        "name": "Mi Gente"
+      }
+    },
+    {
+      "track": {
+        "id": "26spalP6x2gUAab8rwB8dQ",
+        "name": "Gentleman"
+      }
+    },
+    {
+      "track": {
+        "id": "6H7fLdt0AeWpuxUKXuXWrx",
+        "name": "Shree Hanuman Chalisa"
+      }
+    },
+    {
+      "track": {
+        "id": "3V8UKqhEK5zBkBb6d6ub8i",
+        "name": "Te Boté - Remix"
+      }
+    },
+    {
+      "track": {
+        "id": "3dVvWnj4D8JGkKvo6Hucso",
+        "name": "TROLLZ (with Nicki Minaj)"
+      }
+    },
+    {
+      "track": {
+        "id": "1AKGJQIHP3dIPuhOjdxD6a",
+        "name": "Bitch Lasagna"
+      }
+    },
+    {
+      "track": {
+        "id": "0MqK850eOYLmO81rOpEARi",
+        "name": "Lehanga by Jass Manak - Remix"
+      }
+    },
+    {
+      "track": {
+        "id": "5jE48hhRu8E6zBDPRSkEq7",
+        "name": "All About That Bass"
+      }
+    },
+    {
+      "track": {
+        "id": "6mKs2kkBgvf090H566n1pd",
+        "name": "BEBE"
+      }
+    },
+    {
+      "track": {
+        "id": "1Yk0cQdMLx5RzzFTYwmuld",
+        "name": "Hello"
+      }
+    },
+    {
+      "track": {
+        "id": "190jyVPHYjAqEaOGmMzdyk",
+        "name": "Beauty And A Beat"
+      }
+    },
+    {
+      "track": {
+        "id": "30GqVivI3eb7OgonUemcjf",
+        "name": "Joga O Bum Bum Tam Tam"
+      }
+    },
+    {
+      "track": {
+        "id": "5iwz1NiezX7WWjnCgY5TH4",
+        "name": "Calma - Remix"
+      }
+    },
+    {
+      "track": {
+        "id": "7k4t7uLgtOxPwTpFmtJNTY",
+        "name": "Tusa"
+      }
+    },
+    {
+      "track": {
+        "id": "3mJ5dNz3VOpd1ww5Ared5w",
+        "name": "Con Calma"
+      }
+    },
+    {
+      "track": {
+        "id": "2AIWoHr9DF6y4KALCBKWQS",
+        "name": "IDOL"
+      }
+    },
+    {
+      "track": {
+        "id": "5yY9lUy8nbvjM1Uyo1Uqoc",
+        "name": "Life Is Good (feat. Drake)"
+      }
+    },
+    {
+      "track": {
+        "id": "2Cd9iWfcOpGDHLz6tVA3G4",
+        "name": "Waka Waka (This Time for Africa) [The Official 2010 FIFA World Cup (TM) Song] (feat. Freshlyground)"
+      }
+    },
+    {
+      "track": {
+        "id": "5HOpkTTVcmZHnthgyxrIL8",
+        "name": "The Fox (What Does the Fox Say?)"
+      }
+    },
+    {
+      "track": {
+        "id": "2qG5sZ7Si6sdK74qLxedYM",
+        "name": "Con Altura"
+      }
+    },
+    {
+      "track": {
+        "id": "6mICuAdrwEjh6Y6lroV2Kg",
+        "name": "Chantaje (feat. Maluma)"
+      }
+    },
+    {
+      "track": {
+        "id": "7fa9MBXhVfQ8P8Df9OEbD8",
+        "name": "Girls Like You (feat. Cardi B) - Cardi B Version"
+      }
+    },
+    {
+      "track": {
+        "id": "2ijef6ni2amuunRoKTlgww",
+        "name": "Sin Pijama"
+      }
+    },
+    {
+      "track": {
+        "id": "0b9oOr2ZgvyQu88wzixux9",
+        "name": "This Is America"
+      }
+    },
+    {
+      "track": {
+        "id": "63MvWd6T6yoS7h4AJ4Hjrm",
+        "name": "Aankh Marey (From \"Simmba\")"
+      }
+    },
+    {
+      "track": {
+        "id": "2XU0oxnq2qxCpomAAuJY8K",
+        "name": "Dance Monkey"
+      }
+    },
+    {
+      "track": {
+        "id": "6SsasCL9Raj485Xgj6mDKv",
+        "name": "My Moment (Originally Performed by Rebecca Black) - Vocal Version"
+      }
+    },
+    {
+      "track": {
+        "id": "0JoHqmlqE0W0i9prt6kcHR",
+        "name": "Cuatro Babys (feat. Trap Capos, Noriel, Bryant Myers & Juhn)"
+      }
+    },
+    {
+      "track": {
+        "id": "0mJTAdmY8olbGQjopDYff3",
+        "name": "Vaaste"
+      }
+    },
+    {
+      "track": {
+        "id": "5Dq7d6Y2K2CRNFm1bC3I7A",
+        "name": "Bum Bum Bole"
+      }
+    },
+    {
+      "track": {
+        "id": "0Tm5ER6IrGfmW0yZ5lkQli",
+        "name": "Rowdy Baby"
+      }
+    },
+    {
+      "track": {
+        "id": "6kex4EBAj0WHXDKZMEJaaF",
+        "name": "Swalla (feat. Nicki Minaj & Ty Dolla $ign)"
+      }
+    },
+    {
+      "track": {
+        "id": "6NfUEDYjLe2Rw7UU1ExpYg",
+        "name": "Я роняю запад"
+      }
+    },
+    {
+      "track": {
+        "id": "6Za3190Sbw39BBC77WSS1C",
+        "name": "Criminal"
+      }
+    },
+    {
+      "track": {
+        "id": "6iaJUPHLONFJ02RTeaMuy8",
+        "name": "Hit or Miss"
+      }
+    },
+    {
+      "track": {
+        "id": "3MEYFivt6bilQ9q9mFWZ4g",
+        "name": "Alone"
+      }
+    },
+    {
+      "track": {
+        "id": "2AY1UAimvTqjJC8vDJsOyy",
+        "name": "Mayores"
+      }
+    },
+    {
+      "track": {
+        "id": "1u8c2t2Cy7UBoG4ArRcF5g",
+        "name": "Blank Space"
+      }
+    },
+    {
+      "track": {
+        "id": "3DXncPQOG4VBw3QHh3S817",
+        "name": "I'm the One (feat. Justin Bieber, Quavo, Chance the Rapper & Lil Wayne)"
+      }
+    },
+    {
+      "track": {
+        "id": "31EqumBCMNGK3kt1sSL4gV",
+        "name": "Laung Laachi Title Track (From \"Laung Laachi\")"
+      }
+    },
+    {
+      "track": {
+        "id": "6eDApnV9Jdb1nYahOlbbUh",
+        "name": "One Time"
+      }
+    },
+    {
+      "track": {
+        "id": "5GYbkDveRD2I8M5ZJ14hWn",
+        "name": "Never Say Never"
+      }
+    },
+    {
+      "track": {
+        "id": "2hl6q70unbviGo3g1R7uFx",
+        "name": "Échame La Culpa"
+      }
+    },
+    {
+      "track": {
+        "id": "15Uxmx6D6hW0gY5kc7bpqU",
+        "name": "Its Every Night Sis"
+      }
+    }
+  ]
+}
---
+++ yes.py
@@ -0,0 +1,604 @@
+yes_ids ={
+  "items": [
+    {
+      "track": {
+        "id": "7fBv7CLKzipRk6EC6TWHOB",
+        "name": "The Hills"
+      }
+    },
+    {
+      "track": {
+        "id": "3zkWCteF82vJwv0hRLba76",
+        "name": "Stitches"
+      }
+    },
+    {
+      "track": {
+        "id": "7vKfym6hgeJxxyyVpiXBBX",
+        "name": "Animals"
+      }
+    },
+    {
+      "track": {
+        "id": "1rfofaqEpACxVEHIZBJe6W",
+        "name": "Havana (feat. Young Thug)"
+      }
+    },
+    {
+      "track": {
+        "id": "4SFknyjLcyTLJFPKD2m96o",
+        "name": "How You Like That"
+      }
+    },
+    {
+      "track": {
+        "id": "3KkXRkHbMCARz0aVfEt68P",
+        "name": "Sunflower - Spider-Man: Into the Spider-Verse"
+      }
+    },
+    {
+      "track": {
+        "id": "0nrRP2bk19rLc0orkWPQk2",
+        "name": "Wake Me Up"
+      }
+    },
+    {
+      "track": {
+        "id": "2nLtzopw4rPReszdYBJU6h",
+        "name": "Numb"
+      }
+    },
+    {
+      "track": {
+        "id": "1zB4vmk8tFRmM9UULNzbLB",
+        "name": "Thunder"
+      }
+    },
+    {
+      "track": {
+        "id": "7bYZBVrnRfqeaPbhRyEvK3",
+        "name": "Thrift Shop (feat. Wanz)"
+      }
+    },
+    {
+      "track": {
+        "id": "6WaogiItGdUTm490XN4QKJ",
+        "name": "Congratulations"
+      }
+    },
+    {
+      "track": {
+        "id": "0H4ugk6rhnXmTl47ayy9O5",
+        "name": "Daechwita"
+      }
+    },
+    {
+      "track": {
+        "id": "5AgsDLaR50nwwb6UUrQ5pD",
+        "name": "Yummy"
+      }
+    },
+    {
+      "track": {
+        "id": "1i1fxkWeaMmKEB4T7zqbzK",
+        "name": "Don't Let Me Down"
+      }
+    },
+    {
+      "track": {
+        "id": "08bNPGLD8AhKpnnERrAc6G",
+        "name": "FRIENDS"
+      }
+    },
+    {
+      "track": {
+        "id": "3Ys2PYl1wyPKQIwyqhP9cQ",
+        "name": "Stay Gold"
+      }
+    },
+    {
+      "track": {
+        "id": "2S1LebN6AXXQqJolBxlWgO",
+        "name": "Earth"
+      }
+    },
+    {
+      "track": {
+        "id": "2wVDWtLKXunswWecARNILj",
+        "name": "SOLO"
+      }
+    },
+    {
+      "track": {
+        "id": "4tCtwWceOPWzenK2HAIJSb",
+        "name": "Work from Home (feat. Ty Dolla $ign)"
+      }
+    },
+    {
+      "track": {
+        "id": "44ONERBHALxftQNWq6dmyd",
+        "name": "Side To Side"
+      }
+    },
+    {
+      "track": {
+        "id": "3U4isOIWM3VvDubwSI3y7a",
+        "name": "All of Me"
+      }
+    },
+    {
+      "track": {
+        "id": "0cqRj7pUJDkTCEsJkx8snD",
+        "name": "Shake It Off"
+      }
+    },
+    {
+      "track": {
+        "id": "0e7ipj03S05BNilyu5bRzt",
+        "name": "rockstar (feat. 21 Savage)"
+      }
+    },
+    {
+      "track": {
+        "id": "15JINEqzVMv3SvJTAXAKED",
+        "name": "Love The Way You Lie"
+      }
+    },
+    {
+      "track": {
+        "id": "1qDrWA6lyx8cLECdZE7TV7",
+        "name": "Somebody That I Used To Know"
+      }
+    },
+    {
+      "track": {
+        "id": "0KKkJNfGyhkQ5aFogxQAPU",
+        "name": "That's What I Like"
+      }
+    },
+    {
+      "track": {
+        "id": "5iwz1NiezX7WWjnCgY5TH4",
+        "name": "Calma - Remix"
+      }
+    },
+    {
+      "track": {
+        "id": "1P17dC1amhFzptugyAO7Il",
+        "name": "Look What You Made Me Do"
+      }
+    },
+    {
+      "track": {
+        "id": "386RUes7n1uM1yfzgeUuwp",
+        "name": "The Lazy Song"
+      }
+    },
+    {
+      "track": {
+        "id": "4VrWlk8IQxevMvERoX08iC",
+        "name": "Chandelier"
+      }
+    },
+    {
+      "track": {
+        "id": "2QyuXBcV1LJ2rq01KhreMF",
+        "name": "ON"
+      }
+    },
+    {
+      "track": {
+        "id": "535BPG5ftcJeBC0UK6WNbF",
+        "name": "Con Calma"
+      }
+    },
+    {
+      "track": {
+        "id": "4NhDYoQTYCdWHTvlbGVgwo",
+        "name": "GOOBA"
+      }
+    },
+    {
+      "track": {
+        "id": "5RGf8qn1TBWVplLyyKUsuV",
+        "name": "Blood Sweat & Tears"
+      }
+    },
+    {
+      "track": {
+        "id": "0Cy7wt6IlRfBPHXXjmZbcP",
+        "name": "Love Me Like You Do"
+      }
+    },
+    {
+      "track": {
+        "id": "1j4kHkkpqZRBwE0A4CN4Yv",
+        "name": "Dusk Till Dawn - Radio Edit"
+      }
+    },
+    {
+      "track": {
+        "id": "4qefHyLKbyW3yeqk5Jrjey",
+        "name": "A Thousand Years"
+      }
+    },
+    {
+      "track": {
+        "id": "43zdsphuZLzwA9k4DJhU0I",
+        "name": "when the party's over"
+      }
+    },
+    {
+      "track": {
+        "id": "2dpaYNEQHiRxtZbfNsse99",
+        "name": "Happier"
+      }
+    },
+    {
+      "track": {
+        "id": "0Bo5fjMtTfCD8vHGebivqc",
+        "name": "Axel F"
+      }
+    },
+    {
+      "track": {
+        "id": "5knuzwU65gJK7IF5yJsuaW",
+        "name": "Rockabye (feat. Sean Paul & Anne-Marie)"
+      }
+    },
+    {
+      "track": {
+        "id": "43ZyHQITOjhciSUUNPVRHc",
+        "name": "Gucci Gang"
+      }
+    },
+    {
+      "track": {
+        "id": "06KyNuuMOX1ROXRhj787tj",
+        "name": "We Don't Talk Anymore (feat. Selena Gomez)"
+      }
+    },
+    {
+      "track": {
+        "id": "3ee8Jmje8o58CHK66QrVC2",
+        "name": "SAD!"
+      }
+    },
+    {
+      "track": {
+        "id": "13MF2TYuyfITClL1R2ei6e",
+        "name": "BOOMBAYAH"
+      }
+    },
+    {
+      "track": {
+        "id": "1u8c2t2Cy7UBoG4ArRcF5g",
+        "name": "Blank Space"
+      }
+    },
+    {
+      "track": {
+        "id": "2QyuXBcV1LJ2rq01KhreMF",
+        "name": "ON"
+      }
+    },
+    {
+      "track": {
+        "id": "7BKLCZ1jbUBVqRi2FVlTVw",
+        "name": "Closer"
+      }
+    },
+    {
+      "track": {
+        "id": "1AKGJQIHP3dIPuhOjdxD6a",
+        "name": "Bitch Lasagna"
+      }
+    },
+    {
+      "track": {
+        "id": "0F7FA14euOIX8KcbEturGH",
+        "name": "Old Town Road"
+      }
+    },
+    {
+      "track": {
+        "id": "3e9HZxeyfWwjeyPAMmWSSQ",
+        "name": "thank u, next"
+      }
+    },
+    {
+      "track": {
+        "id": "4Ws314Ylb27BVsvlZOy30C",
+        "name": "Lovesick Girls"
+      }
+    },
+    {
+      "track": {
+        "id": "6i0V12jOa3mr6uu4WYhUBr",
+        "name": "Heathens"
+      }
+    },
+    {
+      "track": {
+        "id": "7DFNE7NO0raLIUbgzY2rzm",
+        "name": "Let Her Go"
+      }
+    },
+    {
+      "track": {
+        "id": "34gCuhDGsG4bRPIf9bb02f",
+        "name": "Thinking out Loud"
+      }
+    },
+    {
+      "track": {
+        "id": "6k2900NjJeVqZM55G3Danu",
+        "name": "Bum Bum Tam Tam"
+      }
+    },
+    {
+      "track": {
+        "id": "2XU0oxnq2qxCpomAAuJY8K",
+        "name": "Dance Monkey"
+      }
+    },
+    {
+      "track": {
+        "id": "5tq6gO0LuNjmo9SRT1UcMv",
+        "name": "Dame Tu Cosita"
+      }
+    },
+    {
+      "track": {
+        "id": "6ocbgoVGwYJhOv1GgI9NsF",
+        "name": "7 rings"
+      }
+    },
+    {
+      "track": {
+        "id": "5jrdCoLpJSvHHorevXBATy",
+        "name": "Dark Horse"
+      }
+    },
+    {
+      "track": {
+        "id": "0JP9xo3adEtGSdUEISiszL",
+        "name": "Moonlight"
+      }
+    },
+    {
+      "track": {
+        "id": "27tNWlhdAryQY04Gb2ZhUI",
+        "name": "Roar"
+      }
+    },
+    {
+      "track": {
+        "id": "2iuZJX9X9P0GKaE93xcPjk",
+        "name": "Sugar"
+      }
+    },
+    {
+      "track": {
+        "id": "2tpWsVSb9UEmDRxAl1zhX1",
+        "name": "Counting Stars"
+      }
+    },
+    {
+      "track": {
+        "id": "6DCZcSspjsKoFjzjrWoCdn",
+        "name": "God's Plan"
+      }
+    },
+    {
+      "track": {
+        "id": "2Cd9iWfcOpGDHLz6tVA3G4",
+        "name": "Waka Waka (This Time for Africa) [The Official 2010 FIFA World Cup (TM) Song] (feat. Freshlyground)"
+      }
+    },
+    {
+      "track": {
+        "id": "75scDPqGs75FotglJSoOI2",
+        "name": "MIC Drop (Steve Aoki Remix) [Full Length Edition]"
+      }
+    },
+    {
+      "track": {
+        "id": "2YWjW3wwQIBLNhxWKBQd16",
+        "name": "Lean On (feat. MØ & DJ Snake)"
+      }
+    },
+    {
+      "track": {
+        "id": "2ekn2ttSfGqwhhate0LSR0",
+        "name": "New Rules"
+      }
+    },
+    {
+      "track": {
+        "id": "09CtPGIpYB4BrO8qb1RGsF",
+        "name": "Sorry"
+      }
+    },
+    {
+      "track": {
+        "id": "0RGp4KA9wvndxqPIWoKwnD",
+        "name": "Yalgaar"
+      }
+    },
+    {
+      "track": {
+        "id": "249gnXrbfmV8NG6jTEMSwD",
+        "name": "Life Goes On"
+      }
+    },
+    {
+      "track": {
+        "id": "3CRDbSIZ4r5MsZ0YwxuEkn",
+        "name": "Stressed Out"
+      }
+    },
+    {
+      "track": {
+        "id": "0tgVpDi06FyKpA1z0VMD4v",
+        "name": "Perfect"
+      }
+    },
+    {
+      "track": {
+        "id": "7COfe3P7KgfwDwIRB8LIDw",
+        "name": "Mi Gente"
+      }
+    },
+    {
+      "track": {
+        "id": "2AIWoHr9DF6y4KALCBKWQS",
+        "name": "IDOL"
+      }
+    },
+    {
+      "track": {
+        "id": "0KoiMHhqKLoL4xM54rcWYY",
+        "name": "Taki Taki (feat. Selena Gomez, Ozuna & Cardi B)"
+      }
+    },
+    {
+      "track": {
+        "id": "6or1bKJiZ06IlK0vFvY75k",
+        "name": "Rap God"
+      }
+    },
+    {
+      "track": {
+        "id": "3aW0ds4A4tSQDIp75FqWTo",
+        "name": "FAKE LOVE"
+      }
+    },
+    {
+      "track": {
+        "id": "2Fxmhks0bxGSBdJ92vM42m",
+        "name": "bad guy"
+      }
+    },
+    {
+      "track": {
+        "id": "4JUPEh2DVSXFGExu4Uxevz",
+        "name": "Ice Cream (with Selena Gomez)"
+      }
+    },
+    {
+      "track": {
+        "id": "32OlwWuMpZ6b0aN2RZOeMS",
+        "name": "Uptown Funk (feat. Bruno Mars)"
+      }
+    },
+    {
+      "track": {
+        "id": "0pqnGHJpmpxLKifKRmU6WP",
+        "name": "Believer"
+      }
+    },
+    {
+      "track": {
+        "id": "1Yk0cQdMLx5RzzFTYwmuld",
+        "name": "Hello"
+      }
+    },
+    {
+      "track": {
+        "id": "0u2P5u6lvoDfwTYjAADbn4",
+        "name": "lovely (with Khalid)"
+      }
+    },
+    {
+      "track": {
+        "id": "4lQsB3ERTWSNaAN1IkuNRl",
+        "name": "DDU-DU DDU-DU"
+      }
+    },
+    {
+      "track": {
+        "id": "6epn3r7S14KUqlReYr77hA",
+        "name": "Baby"
+      }
+    },
+    {
+      "track": {
+        "id": "0TK2YIli7K1leLovkQiNik",
+        "name": "Señorita"
+      }
+    },
+    {
+      "track": {
+        "id": "7npLlaPu9Mfno8hjk5OagD",
+        "name": "Girls Like You"
+      }
+    },
+    {
+      "track": {
+        "id": "6Z7m5DLNPOaPg9T6nXK0rQ",
+        "name": "DNA"
+      }
+    },
+    {
+      "track": {
+        "id": "6hvczQ05jc1yGlp9zhb95V",
+        "name": "Kill This Love"
+      }
+    },
+    {
+      "track": {
+        "id": "4SFknyjLcyTLJFPKD2m96o",
+        "name": "How You Like That"
+      }
+    },
+    {
+      "track": {
+        "id": "3MEYFivt6bilQ9q9mFWZ4g",
+        "name": "Alone"
+      }
+    },
+    {
+      "track": {
+        "id": "03UrZgTINDqvnUMbbIMhql",
+        "name": "Gangnam Style (강남스타일)"
+      }
+    },
+    {
+      "track": {
+        "id": "5KawlOMHjWeUjQtnuRs22c",
+        "name": "Boy With Luv (feat. Halsey)"
+      }
+    },
+    {
+      "track": {
+        "id": "698ItKASDavgwZ3WjaWjtz",
+        "name": "Faded"
+      }
+    },
+    {
+      "track": {
+        "id": "7qiZfU4dY1lWllzX7mPBI3",
+        "name": "Shape of You"
+      }
+    },
+    {
+      "track": {
+        "id": "4saklk6nie3yiGePpBwUoc",
+        "name": "Dynamite"
+      }
+    },
+    {
+      "track": {
+        "id": "2JzZzZUQj3Qff7wapcbKjc",
+        "name": "See You Again (feat. Charlie Puth)"
+      }
+    },
+    {
+      "track": {
+        "id": "6habFhsOp2NvshLv26DqMb",
+        "name": "Despacito"
+      }
+    }
+  ]
+}
